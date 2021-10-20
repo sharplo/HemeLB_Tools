4c6ff3ddff42f0c961c47b181d97831cdf97ee52
sharplo <uccacyl@ucl.ac.uk>
2021-10-20 09:53:59 +0100

    written class Bifurcation

--- Verification/PipeFlow.py
+++ Verification/PipeFlow.py
@@ -12,7 +12,7 @@ mu = 0.004 # dynamic viscosity (Pa*s)
 PI = 3.14159265358979323846264338327950288
 
 class PipeFlow(Visual, DiscError):
-    def __init__(self, dx, dt):
+    def __init__(self, dx, dt, dfDict):
         Visual.__init__(self)
         DiscError.__init__(self)
 
@@ -20,16 +20,27 @@ class PipeFlow(Visual, DiscError):
         self.shotBeg = int(sys.argv[2])
         self.shotEnd = int(sys.argv[3])
         
-        self.dfDict = None # dictionary between data frame names and data file names
+        self.dfDict = dfDict # dictionary between data frame names and data file names
         self.dt = dt # step_length (s)
         self.dx = dx # voxel_size (m)
         self.R = None # radius of pipe (m)
         self.P_in = [] # pressure at inlets (mmHg)
         self.P_out = [] # pressure at outlets (mmHg)
 
+        # General procedures
+        self.MakeDataFrames()
+        self.ExtractParams()
+        self.DeriveParams()
+
     def MakeDataFrames(self):
         for key in self.dfDict.keys():
-            setattr(self, key, self.LoadData(self.dfDict[key]))
+            values = self.dfDict[key]
+            if type(values) is str:
+                # Load data from file
+                setattr(self, key, self.LoadData(values))
+            elif type(values) is list:
+                # Intersect data frames
+                setattr(self, key, self.JoinDataFrames(values))
             setattr(getattr(self, key), 'name', key) # df.name = key
             self.RenameCol(getattr(self, key))
 
@@ -40,40 +51,61 @@ class PipeFlow(Visual, DiscError):
             df = df.append(pd.read_csv(self.dir + file + '/' + file + str(shot) + '.txt', delimiter=' '))
         return df
 
+    def JoinDataFrames(self, values):
+        df = getattr(self, values[0])
+        for i in range(1, len(values)):
+            if values[i] == 'cEN':
+                df = self.CentrePoint(df)
+            else:
+                df = pd.merge(df, getattr(self, values[i]))
+        return df
+    
+    def CentrePoint(self, df):
+        return df[(df['grid_x'] > df['grid_x'].mean() - 0.5 * self.dx) \
+            & (df['grid_x'] < df['grid_x'].mean() + 0.5 * self.dx) \
+            & (df['grid_y'] > df['grid_y'].mean() - 0.5 * self.dx) \
+            & (df['grid_y'] < df['grid_y'].mean() + 0.5 * self.dx) \
+            & (df['grid_z'] > df['grid_z'].mean() - 0.5 * self.dx) \
+            & (df['grid_z'] < df['grid_z'].mean() + 0.5 * self.dx)]
+
     def RenameCol(self, df):
         df.rename(columns={
                         'velocity(0)':'Ux',
                         'velocity(1)':'Uy',
                         'velocity(2)':'Uz',
                         'pressure':'P',}, inplace=True)
+
+    def ExtractParams(self):
+        pass # defined in each daughter calss
+
+    def DeriveParams(self):
+        pass # defined in each daughter class
+
         
 class Poiseuille(PipeFlow):
-    def __init__(self, dx, dt):
-        PipeFlow.__init__(self, dx, dt)
+    def __init__(self, dx, dt, dfDict=None):
+        if dfDict == None:
+            dfDict = {'iN':'inlet', 'oUT':'outlet', 'cL':'centreLine', 'pZ':'planeZ'}
+        PipeFlow.__init__(self, dx, dt, dfDict)
 
-        self.dfDict = {'iN':'inlet', 'oUT':'outlet', 'cL':'centreLine', 'pZ':'planeZ'}
-        self.MakeDataFrames()
-        self.ExtractParam()
-        self.DeriveParam()
         for key in self.dfDict.keys():
             self.AddRadialDistance(getattr(self, key))
-
         self.CalExact(self.cL, 'P')
         self.CalExact(self.cL, 'Uz')
         self.CalExact(self.pZ, 'P')
         self.CalExact(self.pZ, 'Uz')
 
-    def ExtractParam(self):
-        self.x_min = self.pZ['grid_x'].min() # 0.0002 (m)
-        self.x_max = self.pZ['grid_x'].max() # 0.0041 (m)
+    def ExtractParams(self):
+        self.x_min = self.iN['grid_x'].min() # 0.0002 (m)
+        self.x_max = self.iN['grid_x'].max() # 0.0041 (m)
         self.z_min = self.cL['grid_z'].min() # 0.0002 (m)
         self.z_max = self.cL['grid_z'].max() # 0.0201 (m)
-        self.P_in.append(self.cL['P'].min()) # 0 (mmHg)
-        self.P_out.append(self.cL['P'].max()) # 0.01 (mmHg)
+        self.P_in.append(self.iN['P'].max()) # 0 (mmHg)
+        self.P_out.append(self.oUT['P'].min()) # 0.01 (mmHg)
 
-    def DeriveParam(self):
-        self.P_min = self.P_in[0]
-        self.P_max = self.P_out[0]
+    def DeriveParams(self):
+        self.P_min = min(self.P_out)
+        self.P_max = max(self.P_in)
         self.L = self.z_max - self.z_min + self.dx # length of pipe (m)
         self.R = 0.5 * (self.x_max - self.x_min + self.dx) # radius of pipe (m)
         self.R0 = 0.5 * (self.x_min + self.x_max) # x-ord of centre
@@ -93,7 +125,22 @@ class Poiseuille(PipeFlow):
         df['err_' + var] = df[var] - df['exSol_' + var]
 
     def CompareExSol_1D(self, df, grid, var1, kwargs1=..., kwargs2=...):
-        return super().Visualise_1D(df, grid, var1, 'exSol_' + var1, kwargs1={'label': 'appSol'}, kwargs2={'label': 'exSol'})
+        return super().Visualise_1D(df, grid, var1, 'exSol_' + var1, kwargs1={'label':'appSol'}, kwargs2={'label':'exSol'})
 
     def CompareExSol_2D(self, df, grid_1, grid_2, var1, var2=None):
         return super().Visualise_2D(df, grid_1, grid_2, var1, var2='exSol_' + var1)
+
+class Bifurcation(PipeFlow):
+    def __init__(self, dx, dt, dfDict=None):
+        if dfDict == None:
+            dfDict = {'iN':'inlet', 'pY':'planeY', 'pZOUT0':'planeZ_out0', \
+                'pZOUT1':'planeZ_out1', 'iNpYcEN':['iN', 'pY', 'cEN'], \
+                'pZOUT0pYcEN':['pZOUT0', 'pY', 'cEN'], 'pZOUT1pYcEN':['pZOUT1', 'pY', 'cEN']}
+        PipeFlow.__init__(self, dx, dt, dfDict)
+
+    def ExtractParams(self):
+        self.x_min = self.iN['grid_x'].min() # (m)
+        self.x_max = self.iN['grid_x'].max() # (m)
+
+    def DeriveParams(self):
+        self.R = 0.5 * (self.x_max - self.x_min + self.dx) # radius of pipe (m)
--- Verification/main.py
+++ Verification/main.py
@@ -1,11 +1,21 @@
 # Local modules
 from PipeFlow import *
 
+"""
 obj = Poiseuille(5e-5, 5e-5)
 obj.CompareExSol_1D(obj.cL, 'grid_z', 'Uz')
+obj.CompareExSol_1D(obj.cL, 'grid_z', 'P')
 obj.Visualise_2D(obj.pZ, 'grid_x', 'grid_y', 'Uz')
 obj.Visualise_2D(obj.pZ, 'grid_x', 'grid_y', 'Uz', 'Ux')
 obj.CompareExSol_2D(obj.pZ, 'grid_x', 'grid_y', 'Uz')
 obj.Visualise_TimeSeries(obj.pZ, 'P', 'Ux')
 obj.WriteDiscErr(obj.cL, 'P')
 obj.WriteDiscErr(obj.pZ, 'Uz')
+"""
+
+obj = Bifurcation(dx=5e-5, dt=1e-5)
+obj.Visualise_TimeSeries(obj.iNpYcEN, 'P', 'Uz')
+obj.Visualise_TimeSeries(obj.pZOUT0pYcEN, 'P', 'Uz')
+obj.Visualise_TimeSeries(obj.pZOUT1pYcEN, 'P', 'Uz')
+obj.Compare_TimeSeries(obj.iNpYcEN, obj.pZOUT0pYcEN, 'P', 'Uz')
+obj.Compare_TimeSeries(obj.iNpYcEN, obj.pZOUT1pYcEN, 'P', 'Uz')
