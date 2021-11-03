bb57bfeb0edae3cf9df5171f405515075be9c883
sharplo <uccacyl@ucl.ac.uk>
2021-11-03 10:40:15 +0000

    written Clustering

--- Verification/PipeFlow.py
+++ Verification/PipeFlow.py
@@ -27,21 +27,25 @@ class PipeFlow(Visual, DiscError):
         self.P_out = [] # pressure at outlets (mmHg)
 
         # General procedures
-        self.MakeDataFrames()
+        for key in self.dfDict.keys():
+            values = self.dfDict[key]
+            self.MakeDataFrames(key, values)
         self.ExtractParams()
         self.DeriveParams()
 
-    def MakeDataFrames(self):
-        for key in self.dfDict.keys():
-            values = self.dfDict[key]
-            if type(values) is str:
-                # Load data from file
-                setattr(self, key, self.LoadData(values))
-            elif type(values) is list:
-                # Intersect data frames
-                setattr(self, key, self.JoinDataFrames(values))
-            setattr(getattr(self, key), 'name', key) # df.name = key
-            self.RenameCol(getattr(self, key))
+    def MakeDataFrames(self, key, values):
+        if type(values) is str:
+            # Load data from file
+            setattr(self, key, self.LoadData(values))
+        elif type(values) is list:
+            # Intersect data frames
+            setattr(self, key, self.JoinDataFrames(values))
+        setattr(getattr(self, key), 'name', key) # df.name = key
+        self.RenameCol(getattr(self, key))
+    
+    def AddDataFrame(self, key, values):
+        self.MakeDataFrames(key, values)
+        self.dfDict[key] = values
 
     def LoadData(self, file):
         df = pd.DataFrame()
@@ -51,10 +55,13 @@ class PipeFlow(Visual, DiscError):
         return df
 
     def JoinDataFrames(self, values):
+        # Note that the new dataframe points to the base dataframe
         df = getattr(self, values[0])
         for i in range(1, len(values)):
             if values[i] == 'cEN':
                 df = self.CentrePoint(df)
+            elif type(values[i]) == int:
+                df = df[df['cluster'] == values[i]]
             else:
                 df = pd.merge(df, getattr(self, values[i]))
         return df
@@ -80,6 +87,48 @@ class PipeFlow(Visual, DiscError):
     def DeriveParams(self):
         pass # defined in each daughter class
 
+    def Clustering(self, df, position):
+
+        num_plane = len(position)
+        num_row = len(df)
+
+        # Convert from lattice unit to physical unit
+        position = self.dx * position
+
+        # Calculate distance from each given position
+        grid = np.array([df['grid_x'], df['grid_y'], df['grid_z']]).transpose()
+        distance = np.array([])
+        for i in range(num_plane):
+            # Using axis 0 is the most efficient
+            dist = np.linalg.norm(np.transpose(grid - position[i,:]), axis=0)
+            distance = np.append(distance, dist)
+        distance = distance.reshape(num_plane, num_row)
+
+        planeIdx = np.argmin(distance, axis=0)
+
+        # Alternative implementation
+        '''
+        planeIdx = np.array([], dtype=np.int)
+        for x, y, z in zip(df['grid_x'], df['grid_y'], df['grid_z']):
+            grid = np.array([x, y, z])
+            distMin = np.inf
+            for i in range(num_plane):
+                dist = np.linalg.norm(grid - position[i])
+                if (dist < distMin):
+                    distMin = dist
+                    num = i
+            planeIdx = np.append(planeIdx, num)
+        '''
+
+        # Record results
+        df['cluster'] = planeIdx
+
+        # Make dataframe for each cluster
+        for i in range(num_plane):
+            key = df.name + str(i)
+            values = [df.name, i]
+            self.AddDataFrame(key, values)
+            setattr(getattr(self, key), 'position', position[i])
         
 class Poiseuille(PipeFlow):
     def __init__(self, dx, dt, dfDict=None):
--- Verification/main.py
+++ Verification/main.py
@@ -12,7 +12,7 @@ obj.Visualise_TimeSeries(obj.pZ, 'P', 'Uz')
 obj.WriteDiscErr(obj.cL, 'P')
 obj.WriteDiscErr(obj.pZ, 'Uz')
 
-"""
+
 
 obj = Bifurcation(dx=5e-5, dt=1e-5)
 obj.Visualise_TimeSeries(obj.iNpYcEN, 'P', 'Uz')
@@ -20,3 +20,23 @@ obj.Visualise_TimeSeries(obj.pZOUT0pYcEN, 'P', 'Uz')
 obj.Visualise_TimeSeries(obj.pZOUT1pYcEN, 'P', 'Uz')
 obj.Compare_TimeSeries(obj.iNpYcEN, obj.pZOUT0pYcEN, 'P', 'Uz')
 obj.Compare_TimeSeries(obj.iNpYcEN, obj.pZOUT1pYcEN, 'P', 'Uz')
+
+"""
+
+normal_iN = np.array([[0.866025,-2.70088e-11,-0.5]])
+position_iN = np.array([[8.02016,13.0403,123.482]])
+
+normal_oUT = - np.array([[0.866025,-2.7027e-11,0.5],
+                        [2.06435e-11,-2.48225e-10,-1],
+                        [-1.16783e-12,-6.89079e-12,1],
+                        [-0.866025,-2.69401e-11,-0.5],
+                        [-0.866025,-2.68425e-11,0.5]]) # note the global minus sign
+position_oUT = np.array([[8.02016,13.0403,73.2765],
+                        [51.4991,13.0403,148.584],
+                        [51.4991,13.0403,3],
+                        [94.978,13.0403,123.482],
+                        [94.978,13.0403,73.2765]])
+
+dfDict = {'iN':'inlet', 'oUT':'outlet', 'pY':'planeY'}
+obj = PipeFlow(2e-5, 1e-5, dfDict)
+obj.Clustering(obj.oUT, position_oUT)
