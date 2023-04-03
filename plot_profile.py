a1f541d0133b69aef18a3b13c5f86a9b8e923c9e
Sharp C. Y. Lo <uccacyl@ucl.ac.uk>
2023-04-03 18:33:33 +0100

    add ticks and grids to the inlet velocity plot

--- plot_profile.py
+++ plot_profile.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -6,6 +7,9 @@ file = sys.argv[1]
 
 df = pd.read_table(file, sep=' ', header=None, names=['time', 'Umax'])
 plt.plot(df['time'], df['Umax'])
-plt.xlabel('time (s)')
-plt.ylabel('Umax (m/s)')
+plt.grid()
+plt.minorticks_on()
+plt.xticks(np.arange(df['time'].min(), df['time'].max()+1, 1.0))
+plt.xlabel('Time (s)')
+plt.ylabel('Flow velocity (m/s)')
 plt.savefig(file[:-4] + '.png')
