51380fd64e8266afb8163dd7dbcd6655887964b7
Sharp C. Y. Lo <uccacyl@ucl.ac.uk>
2023-08-12 10:05:42 +0100

    init branch

---
+++ UQ_campaigns/UQAAA/supplement/BoundaryVelocityRatio.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from scipy.special import jv
+
+
+dx = 1.0e-4
+h = 1.775e-3 #m, wall thickness
+PR = 0.5 #Poisson Ratio
+rho = 1050 #kg/m3 fluid density
+rhoS = rho #kg/m3 wall density
+
+
+x=np.linspace(0,0.1)
+
+for r in [13, 20, 27]: #sites per radius
+    for a in [10.1, 12.3]: #womersley number
+        R=r*dx
+        Lambda = (1j**1.5)*a #Wommersley
+
+        g = 2*jv(1,Lambda)/(Lambda*jv(0,Lambda))
+
+        A = (g-1)*(PR**2-1)
+        B = (rhoS*h*(g-1))/(rho*R) + (2*PR-0.5)*g - 2
+        C = 2*rhoS*h/(rho*R) + g
+
+        roots = np.roots([A,B,C])
+        nu = max(roots[0],roots[1])
+
+        M = (2+nu*(2*PR-1))/(nu*(2*PR-g))
+
+        print("M = ", M)
+
+        plt.figure(1)
+        f1 = []
+        for i in x:
+            f1.append((1-M)/(1-M*jv(0,Lambda*(1-i))/jv(0,Lambda)))
+        plt.plot(100*x,f1,label='Wo=%s, r=%s dx' %(a,r))
+        print('f1', min(f1))
+
+plt.legend()
+plt.xlabel('Extension (%)')
+plt.ylabel('F')
+plt.grid()
+plt.minorticks_on()
+plt.savefig("lineF.png")
+
+
+plt.figure(2)
+R = 20*dx #m, Average Wall position
+Extend = np.arange(0.00,0.10,0.0025)
+Wom = np.arange(10.1,12.3,0.2) # Wommersley
+f = np.zeros(len(Extend)*len(Wom))
+i=0
+for e in Extend:
+    for a in Wom:
+        Lambda = (1j**1.5)*a
+
+        g = 2*jv(1,Lambda)/(Lambda*jv(0,Lambda))
+
+				# Solve for nu in a quadratic equation
+        A = (g-1)*(PR**2-1)
+        B = (rhoS*h*(g-1))/(rho*R) + (2*PR-0.5)*g - 2
+        C = 2*rhoS*h/(rho*R) + g
+
+        roots = np.roots([A,B,C])
+        nu = max(roots[0],roots[1])
+
+        M = (2+nu*(2*PR-1))/(nu*(2*PR-g))
+
+        f[i] = ((1-M)/(1-M*jv(0,Lambda*(1-e))/jv(0,Lambda)))
+        i=i+1
+
+print('mean', np.average(f))
+print('median', np.median(f))
+print('min', np.min(f))
+
+plt.imshow(f.reshape(len(Extend),len(Wom)), aspect="auto", extent=[10.1,12.3,0,10], origin='lower')
+plt.colorbar(label='F') 
+plt.xlabel("Womersley Number")
+plt.ylabel("Extension (%)")
+plt.savefig("mapF.png")
--- writeInput.py
+++ writeInput.py
@@ -8,7 +8,8 @@ from MyModules.InputOutput import *
 InOut = InputOutput(sys.argv[1], sys.argv[2])
 
 # General
-param_sim = {'kernel':'LBGK', 'tau':0.56, 'timeSteps':55000}
+param_sim = {'kernel':'TRT', 'tau':0.56, 'time':9.2, 'relaxationParameter':0.25, \
+             'YoungsModulus':9000000, 'BoundaryVelocityRatio':0.45}
 
 # Cylinder
 #param_iN = {'type':'velocity', 'subtype':'parabolic', 'Re':10}
@@ -30,10 +31,10 @@ param_sim = {'kernel':'LBGK', 'tau':0.56, 'timeSteps':55000}
 #    'flowRateRatios':'Murray', 'power':3, 'gamma_R':1, 'gamma_RC':1}
 
 # Abdominal aorta
-param_iN = {'type':'velocity', 'subtype':'file', 'Re':660, 'Wo':12.3, \
+param_iN = {'type':'velocity', 'subtype':'file', 'Re':600, 'Wo':11.2, \
             'profile':'ESM_File2_Q_d4.txt', 'offset':0.2}
 param_oUT = {'type':'pressure', 'subtype':'WK', 'geometry':'0156_0001_7e-3', \
-    'flowRateRatios':'Murray', 'power':3, 'gamma_R':1e3, 'gamma_RC':1e5}
+    'flowRateRatios':'Murray', 'power':2, 'gamma_R':1024, 'gamma_RC':256}
 
 #InOut.RescaleSize(1e-3)
 InOut.ChangeParam(param_sim, param_iN=param_iN, param_oUT=param_oUT)
@@ -45,18 +46,20 @@ InOut.WriteInput(sys.argv[3])
 
 ## Series of experiments
 #--------------------------------------------------------------------------------------
-CaseName = 'ProfundaFemoris2_coarse'
-Exp = pd.read_csv(CaseName + '/experiments.csv')
+CaseName = '0156_0001v3_1e-4'
+Exp = pd.read_csv(CaseName + '/exp.csv')
 InFile = CaseName + '/input_VfWKf.xml'
 OutFilePrefix = '/input_'
 
 for row in Exp.itertuples(index=False):
     print('caseNum', row.caseNum)
     InOut = InputOutput(InFile, CaseName + '/')
-    param_sim = {'tau':0.9082, 'timeSteps':row.timeSteps}
-    param_iN = {'type':'velocity', 'subtype':'file', 'Re':10, 'Wo':2, 'epsilon':0.1}
-    param_oUT = {'type':'pressure', 'subtype':'fileWK', 'geometry':'ProfundaFemoris2_2e-3', \
-    'flowRateRatios':'Murray', 'power':3, 'gamma_R':row.gamma_R, 'gamma_RC':row.gamma_RC}
+    param_sim = {'kernel':'TRT', 'tau':0.56, 'time':9.2, 'relaxationParameter':row.Lambda, \
+                 'YoungsModulus':row.E, 'BoundaryVelocityRatio':row.F}
+    param_iN = {'type':'velocity', 'subtype':'file', 'Re':row.Re, 'Wo':row.Wo, \
+                'profile':'ESM_File2_Q_d4.txt', 'offset':0.2}
+    param_oUT = {'type':'pressure', 'subtype':'WK', 'geometry':'0156_0001_7e-3', \
+                 'flowRateRatios':'Murray', 'power':row.m, 'gamma_R':row.gamma_R, 'gamma_RC':row.gamma_RC}
     InOut.ChangeParam(param_sim, param_iN=param_iN, param_oUT=param_oUT)
     InOut.WriteInput(OutFilePrefix + str(row.caseNum) + '.xml')
 
